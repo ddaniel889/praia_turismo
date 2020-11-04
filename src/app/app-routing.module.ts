73abd72faa8bc01e62fe42bff913d1e872c17cf1
Daniel Carrasco <dcarrasco@novopayment.com>
2020-11-04 02:14:00 -0400

    Archivos de proyecto praia
    folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'; 
+import { MisionComponent } from './pages/mision/mision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'mision', component: MisionComponent},
+  {path:'**', redirectTo:'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import {ServicesModule} from "./services/services.module";
+
+import {SharedModule} from "./shared/shared.module";
+import { MisionComponent } from './pages/mision/mision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MisionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ServicesModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
